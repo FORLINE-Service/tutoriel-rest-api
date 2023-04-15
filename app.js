4a45d6ed9b4e004e149bc6d2d9173369a3f5ade7
samuel-lwanzo <78253795+samuel-lwanzo@users.noreply.github.com>
2023-04-15 13:49:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -18,3 +18,14 @@ connect("mongodb://127.0.0.1:27017/", (err) => {
     console.log("Attente des requêtes au port 3OOO");
   }
 });
+// connect("mongodb+srv://apedh:nitGA05t4rIfpdui@cluster0.zbqhk9u.mongodb.net/test", (err) => {
+//   if (err) {
+//     console.log("Erreur lors de la connexion à la base de données");
+//     process.exit(-1);
+//   } else {
+//     console.log("Connexion avec la base de données établie");
+//     app.listen(3000);
+//     console.log("Attente des requêtes au port 3OOO");
+//   }
+// });
+
