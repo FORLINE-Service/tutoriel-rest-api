6adfeaa6ebaf267748737618918f30d283b157a4
samuel-lwanzo <78253795+samuel-lwanzo@users.noreply.github.com>
2023-04-17 15:43:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://sosv1.netlify.app',
+    origin: 'https://sosv1.netlify.app/',
+    // origin: 'http://sosv1.netlify.app',
     // origin: 'http://localhost:3000',
     credentials: false,
     
